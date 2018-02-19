7282daf80d5ff62ec14aa2cdcadf9abfc15d9377
Informatikai Rendszeruzemelteto <inf.rendsz.2018.13cf@gmail.com>
2018-02-19 21:03:27 +0100

    Added method

--- Program.cs
+++ Program.cs
@@ -4,6 +4,13 @@ namespace MyFirstConsoleApplication
 {
     class Program
     {
-        static void Main(string[] args) => Console.WriteLine("Hello World!");
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!\n" + GetUserName());
+        }
+
+        public static string GetUserName()
+        {
+            return "Nandor Dudas";
+        }
     }
 }
